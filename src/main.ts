b18c985bc55b1a017a065e2b0203128e96d3b848
David Moritz <david@jde-systems.com>
2024-06-19 16:11:38 -0500

    Interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpHandlerFn,
+  type HttpRequest,
+  provideHttpClient,
+  withInterceptors
+} from '@angular/common/http';
+
+function loggingIntercepter(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  console.log('outgoing request', request);
+  const req = request.clone({
+    headers: request.headers.set('X-DEBUG', 'TESTING')
+  });
+  return next(request);
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
+  providers: [provideHttpClient(withInterceptors([loggingIntercepter]))]
 }).catch((err) => console.error(err));
